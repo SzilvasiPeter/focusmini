3673b5703ced1a3757ec0d2f61c7e7e609f2b63c
SzilvasiPeter <peti.szilvasi95@gmail.com>
2026-02-12 11:07:32 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,12 +7,12 @@ if [ "$#" -ne 1 ]; then
 fi
 
 version="$1"
+tag="v$version"
 
 cargo set-version "$version"
-cargo check # ensures Cargo.lock is update
+cargo check # ensures Cargo.lock is updated
 git add Cargo.toml Cargo.lock
-git commit -m "Bump version to v$version"
+git commit -m "Bump version to $tag"
 git push
-tag="v$version"
 git tag "$tag"
 git push origin "$tag"
