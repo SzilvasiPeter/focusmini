d599dd47755781e5b3bb5a864526e535ed2da8f4
Peter Szilvasi <peti.szilvasi95@gmail.com>
2026-02-13 09:53:40 +0000

    chore: make the release script more concise

--- release.sh
+++ release.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$#" -ne 1 ]; then
-  printf 'usage: %s <version>\n' "$0" >&2
-  exit 1
-fi
-
-version="$1"
+version=${1:?usage: $0 <version>}
 tag="v$version"
 
 cargo set-version "$version"
-cargo check # ensures Cargo.lock is updated
 git add Cargo.toml Cargo.lock
 git commit -m "Bump version to $tag"
-git push
 git tag "$tag"
-git push origin "$tag"
+git push origin HEAD "$tag"
